31c219f70a0344cbf8f6bc9f4973a21a3cc5d357
James Cook <cookjws@gmail.com>
2020-07-09 12:44:48 +0100

    refactor of src and bump version number

--- src/Replace.jl
+++ src/Replace.jl
@@ -7,21 +7,6 @@ using Cassette
 
 export @replace
 
-"""
-`@replace foo bar qux` replaces the function `foo` with the function `bar` where it occurs in expression `qux`
-
-```julia
-1 == @replace sin cos sin(0.0)
-```
-"""
-macro replace(this, forthat, inhere)
-  @eval Cassette.@context Ctx
-  @eval Cassette.overdub(ctx::Ctx, fn::typeof($this), args...) = ctx.metadata(args...)
-  quote
-    Cassette.overdub(Ctx(metadata=$(esc(forthat))), () -> $(esc(inhere)))
-  end
-end
-
 function _replace!(ctx::T, mapping) where T
   for (key, value) in mapping
     @eval Cassette.overdub(ctx::$T, fn::typeof($key), args...) = $value(args...)
@@ -29,6 +14,15 @@ function _replace!(ctx::T, mapping) where T
   return ctx
 end
 
+"""
+`@replace foo bar` replaces occurences in expression `bar` of the first item of
+each iteration of `foo` with the second item in each iteration of `foo`.
+
+```julia
+1 == @replace Dict(sin=>cos) sin(0.0)
+exp(0.0) + 1 == @replace Dict(sin=>cos, tan=>exp) sin(0.0) + tan(0.0)
+```
+"""
 macro replace(mapping, ex)
   @eval Cassette.@context Ctx
   quote
@@ -37,5 +31,19 @@ macro replace(mapping, ex)
     Cassette.overdub(ctx, () -> $(esc(ex)))
   end
 end
+
+"""
+`@replace foo bar qux` replaces the function `foo` with the function `bar` where it occurs in expression `qux`
+
+```julia
+1 == @replace sin cos sin(0.0)
+```
+"""
+macro replace(this, forthat, inhere)
+  quote
+    @replace Dict($(esc(this))=>$(esc(forthat))) $(esc(inhere))
+  end
+end
+
 
 end
